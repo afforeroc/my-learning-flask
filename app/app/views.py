0413fd1611763a022f20a94aa0a0b74571be9282
Andres Felipe Forero Correa <afforeroc@unal.edu.co>
2020-05-30 10:20:55 -0500

    Working with forms in Flask | Learning Flask Ep. 7

--- app/app/views.py
+++ app/app/views.py
@@ -2,6 +2,7 @@ from app import app
 
 from flask import render_template
 from datetime import datetime
+from flask import request, redirect
 
 @app.route("/")
 def index():
@@ -82,4 +83,21 @@ def jinja():
 
 @app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+
+    if request.method == "POST":
+
+        req = request.form
+
+        missing = list()
+
+        for k, v in req.items():
+            if v == "":
+                missing.append(k)
+
+        if missing:
+            feedback = f"Missing fields for {', '.join(missing)}"
+            return render_template("public/sign_up.html", feedback=feedback)
+
+        return redirect(request.url)
+
     return render_template("public/sign_up.html")
